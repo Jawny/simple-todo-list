f751e92d4341a8cf87e25a12bf0393e57195a74b
Johnny <johnnyliao06@gmail.com>
2020-03-28 17:34:12 -0700

    upload files

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import TodoList from './TodoList/todoList';
+import AddTodo from './AddTodo/addtodo';
+import './App.css';
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: []
+    };
+  }
+
+  render() {
+    return(
+      <div>
+        <AddTodo addTodoFn={this.addTodo}></AddTodo>
+        <TodoList updateTodoFn={this.updateTodo} todos={this.state.todos}></TodoList>
+      </div>
+    );
+  }
+
+  componentDidMount = () => {
+    const todos = localStorage.getItem('todos');
+    if(todos) {
+      const savedTodos = JSON.parse(todos);
+      this.setState({todos: savedTodos})
+    } else {
+      console.log('no Todos');
+    }
+  }
+
+  addTodo = async (todo) => {
+    await this.setState({todos: [...this.state.todos, {
+      text: todo,
+      completed: false
+    }]});
+    localStorage.setItem('todos', JSON.stringify(this.state.todos));
+    console.log(localStorage.getItem('todos'));
+  }
+
+  updateTodo = (todo) => {
+    const newTodos = this.state.todos.map(_todo => {
+      if(todo === _todo)
+        return{
+          text: todo.text,
+          completed: !todo.completed
+        }
+      else
+        return _todo
+    });
+    // this.setState({todos: newTodos});
+    console.log(newTodos)
+  }
+}
+
+export default App;
